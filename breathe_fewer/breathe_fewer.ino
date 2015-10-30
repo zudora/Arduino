619cf65749b6f1bc6a9dd9783bce93931b879c09
zudora <zudora@users.noreply.github.com>
2015-10-29 22:32:35 -0700

    Random array sort to be used for led ordering

--- breathe_fewer/breathe_fewer.ino
+++ breathe_fewer/breathe_fewer.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 0
+int ledNum[16];
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -13,6 +14,10 @@ void setup() {
   strip.begin();
   //strip.setBrightness(100);
   strip.show(); // Initialize all pixels to 'off'
+  for (int i = 0; i < 16; i++)
+  {
+    ledNum[i] = i;
+  }
 }
 
 void loop() {
@@ -50,13 +55,22 @@ void breatheAll() {
 
 //Use for blue-purple colors appropriate for jellyfish.
 void breatheAllblue() {
-  const in delay_time = 100;
-  const in led_delay = 10;
+  const int delay_time = 100;
+  const int led_delay = 10;
   
   for (int i=5; i <= 127 ; i++) {
+    int r;
+    for (int rand_index = 0; rand_index < 15; rand_index++)
+    {
+      r = random(rand_index, 14);
+      int temp = ledNum[rand_index];
+      ledNum[rand_index] = ledNum[r];
+      ledNum[r] = temp;
+    }
+    
     for(int j=0; j<strip.numPixels(); j++) {      
       strip.setPixelColor(j, strip.Color(i, 0, i*2));
-      delay(led_delay)
+      delay(led_delay);
     }
     strip.show();
     //delay(delay_time);
@@ -64,7 +78,7 @@ void breatheAllblue() {
   for (int i = 127; i >= 5; i--) {
     for(int j=0; j<strip.numPixels(); j++) {
       strip.setPixelColor(j, strip.Color(i, 0, i*2));
-      delay(led_delay)
+      delay(led_delay);
     }    
     strip.show();
     //delay(delay_time);
