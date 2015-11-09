11198a4fd19b2f407b8448fdff12d8ba16bbffb6
zudora <zudora@users.noreply.github.com>
2015-11-08 21:39:35 -0800

    Starting studs program

--- neopixel_studs/neopixel_studs.ino
+++ neopixel_studs/neopixel_studs.ino
@@ -1,8 +1,12 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 0
+#define LED_PIN 0
+#define MIC_PIN 1
+#define DC_OFFSET  0  // DC offset in mic signal - if unusure, leave 0
+#define NOISE     10  // Noise/hum/interference in mic signal
+#define SAMPLES   60  // Length of buffer for dynamic level adjustment
 
-Adafruit_Neopixel strip = Adafruit_Neopixel(2, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_Neopixel strip = Adafruit_Neopixel(2, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // put your setup code here, to run once:
