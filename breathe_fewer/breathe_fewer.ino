9fc570c3ef96e7995ecf3962121172f23315603d
zudora <zudora@users.noreply.github.com>
2016-01-24 15:19:23 -0800

    Breathe adjusted to wipe across the ring from random starting point

--- breathe_fewer/breathe_fewer.ino
+++ breathe_fewer/breathe_fewer.ino
@@ -3,8 +3,8 @@
 #define PIN 0
 
 const int numLeds = 16; 
-
-int ledNum[16];
+const int led_delay = 50;
+ 
 int wipeArray[16] = {8,7,9,6,10,5,11,4,12,3,13,2,14,1,15,0};
 
 // Parameter 1 = number of pixels in strip
@@ -18,48 +18,68 @@ void setup() {
   strip.begin();
   strip.setBrightness(70);
   strip.show(); // Initialize all pixels to 'off'
-  for (int i = 0; i < 16; i++){
-    ledNum[i] = i;
-  }
-  int r;
-  for (int rand_index = 0; rand_index < 14; rand_index++){
-    r = random(rand_index, 15);
-    int temp = ledNum[rand_index];
-    ledNum[rand_index] = ledNum[r];
-    ledNum[r] = temp;
-  }
 }
 
 void loop() {
-
-  breatheAllRand(110);
-  breatheAllRand(127);
-  breatheAllRand(90);
+  pulseAll(110);
+  pulseAll(127);
+  pulseAll(90);
   delay(1000);
 }
 
-//Randomizing next led to update
-void breatheAllRand(int max_red) {
-  const int delay_time = 100;
-  const int led_delay = 50;
+//Currently using wipe to choose next led
+void pulseAll(int max_red) {
   
-  for (int redVal=8; redVal <= max_red ; redVal++) {         
+  for (int redVal=5; redVal <= max_red ; redVal++) {         
     for (int blueAug = 1; blueAug >= 0; blueAug--) {      
-      for(int j=0; j<strip.numPixels(); j++) {              
-        strip.setPixelColor(wipeArray[j], strip.Color(redVal, 0, redVal * 2 - blueAug ));
-        strip.show();
-        delay(led_delay);
-      }
+      updateStrip(redVal, redVal * 2 - blueAug);      
     }    
   }
-  for (int redVal = max_red; redVal >= 8; redVal--) {
-    for (int blueAug = 1; blueAug >= 0; blueAug--) {      
-      for(int j=0; j < strip.numPixels(); j++) {
-        strip.setPixelColor(wipeArray[j], strip.Color(redVal, 0, redVal * 2 + blueAug));
-        strip.show();
-        delay(led_delay);
-      }
-    }   
+  for (int redVal = max_red; redVal >= 5; redVal--) {
+    for (int blueAug = -1; blueAug <= 0; blueAug++) {      
+      updateStrip(redVal, redVal * 2 - blueAug);     
+    }    
+  }
+  if (max_red * 2 < 225) {
+    //Make it bluer
+    int blueVal = max_red * 2;
+    while (blueVal <= 235) {   
+     for (int blueAug = 1; blueAug >= 0; blueAug--) {      
+      updateStrip(max_red, blueVal - blueAug);     
+     }
+     blueVal+=2;
+    }  
   }
   delay(300);
 }
+
+void updateStrip(int redVal, int blueVal) {
+    for(int j=0; j < strip.numPixels(); j++) {
+      strip.setPixelColor(wipeArray[j], strip.Color(redVal, 0, blueVal));
+      strip.show();
+      delay(led_delay);      
+    }
+    genWipeArray();
+}
+
+void genWipeArray() {
+  //numbers 0-15
+  //choose starting point, which is an integer
+  int tempArray[16];  
+  int startNum = random(0, 16);
+  int arrayIndex = 0;
+  tempArray[0] = startNum;
+  
+  for (int ledShift = 1; ledShift <= 8; ledShift++) {
+    arrayIndex++;
+    tempArray[arrayIndex] = (startNum - ledShift) % 16;
+    arrayIndex++;
+    if (ledShift < 8) {
+    tempArray[arrayIndex] = (startNum + ledShift) % 16;
+    }
+  }
+  for (int i = 0; i <= 15; i++) {
+    wipeArray[i] = tempArray[i];
+  }
+}
+
